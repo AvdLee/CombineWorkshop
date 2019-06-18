cfddd4293bb37757d7353f801b025198adabaa4f
Antoine van der Lee <antoine@wetransfer.com>
2019-06-18 20:48:21 +0200

    Implemented signup step 3

--- CombineWorkshop/CombineWorkshop/Steps/StepThreeViewController.swift
+++ CombineWorkshop/CombineWorkshop/Steps/StepThreeViewController.swift
@@ -12,18 +12,70 @@ import Combine
 final class StepThreeViewController: UIViewController {
 
     @IBOutlet private weak var nextButton: UIButton!
-    private var switchesSubscriber: AnyCancellable?
+    private var validationSubscriber: AnyCancellable?
 
     @IBOutlet private weak var usernameTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
     @IBOutlet private weak var passwordConfirmTextField: UITextField!
 
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordAgain: String = ""
+
+    var validatedPassword: AnyPublisher<String?, Never> {
+        return Publishers.CombineLatest($password, $passwordAgain) { password, passwordAgain -> String? in
+            guard password == passwordAgain, password.count > 8 else { return nil }
+            return password
+        }
+        .map { $0 == "password1" ? nil : $0 }
+        .eraseToAnyPublisher()
+    }
+
+    var validatedCredentials: AnyPublisher<(String, String)?, Never> {
+        return Publishers.CombineLatest(validatedUsername, validatedPassword) { username, password -> (String, String)? in
+            guard let uname = username, let pwd = password else { return nil }
+            return (uname, pwd)
+        }
+        .eraseToAnyPublisher()
+    }
+
+    var validatedUsername: AnyPublisher<String?, Never> {
+        return $username
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { username in
+                return Publishers.Future { promise in
+                    self.usernameAvailable(username) { available in
+                        promise(.success(available ? username : nil))
+                    }
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func usernameAvailable(_ username: String, completion: (_ available: Bool) -> Void) {
+        completion(true)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        switchesSubscriber = Publishers.CombineLatest3($switchOneValue, $switchTwoValue, $switchThreeValue, transform: { $0 && $1 && $2 })
-//            .receive(on: RunLoop.main)
-//            .assign(to: \.isEnabled, on: nextButton)
+        self.validationSubscriber = self.validatedCredentials
+            .map { $0 != nil }
+            .receive(on: RunLoop.main)
+            .assign(to: \.isEnabled, on: nextButton)
+    }
+
+    @IBAction func usernameChanged(_ sender: UITextField) {
+        username = sender.text ?? ""
+    }
+
+    @IBAction func passwordChanged(_ sender: UITextField) {
+        password = sender.text ?? ""
+    }
+
+    @IBAction func passwordAgainChanged(_ sender: UITextField) {
+        passwordAgain = sender.text ?? ""
     }
 
 }
